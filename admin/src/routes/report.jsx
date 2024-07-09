553fc4324bd8fa7a2d13d5b71db01939ab5cb4d7
Tahmine Rahbar <rahbar@silicontech.ir>
2024-07-09 10:31:12 +0330

    add excel api in report page

--- admin/src/routes/report.jsx
+++ admin/src/routes/report.jsx
@@ -3,7 +3,7 @@ import { Skeleton } from '../components/shadcn/skeleton'
 import { Button } from '../components/button'
 import persian from 'react-date-object/calendars/persian'
 import persian_fa from 'react-date-object/locales/persian_fa'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 import { Input } from '../components/input'
 import { Label } from '../components/label'
 import DatePicker from 'react-multi-date-picker'
@@ -14,11 +14,72 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '../components/shadcn/tooltip'
+import { BASE_URL } from '../constants/config'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import * as XLSX from 'xlsx'
 
 export function Report() {
   const [isLoading, setIsLoading] = useState(false)
+  const [date, setDate] = useState('')
+  const [date2, setDate2] = useState('')
+  const [username, setUsername] = useState('')
 
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
+
+  function exportUserMonthReport() {
+    axios
+      .get(`${BASE_URL}/system/excel`, {
+        params: {
+          date,
+          username,
+        },
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      })
+      .then((data) => {
+        console.log(data)
+        // create
+        const userReportSheet = XLSX.utils.json_to_sheet(data.data.data)
+        const workbook = XLSX.utils.book_new()
+        // add
+        XLSX.utils.book_append_sheet(workbook, userReportSheet, 'user Sheet')
+        // generate and save
+        XLSX.writeFile(workbook, 'user.xlsx', { compression: true })
+        toast.success('دانلود با موفقیت انجام شد.')
+      })
+      .catch()
+      .finally(() => setIsLoading(false))
+  }
+  function exportCompanyMonthReport() {
+    axios
+      .get(`${BASE_URL}/system/excel`, {
+        params: {
+          date: date2,
+        },
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      })
+      .then((data) => {
+        console.log(data)
+        // create
+        const companyReportSheet = XLSX.utils.json_to_sheet(data.data.data)
+        const workbook = XLSX.utils.book_new()
+        // add
+        XLSX.utils.book_append_sheet(
+          workbook,
+          companyReportSheet,
+          'company Sheet'
+        )
+        // generate and save
+        XLSX.writeFile(workbook, 'company.xlsx', { compression: true })
+        toast.success('دانلود با موفقیت انجام شد.')
+      })
+      .catch()
+      .finally(() => setIsLoading(false))
+  }
 
   useEffect(() => {}, [])
 
@@ -47,6 +108,10 @@ export function Report() {
               <div className="flex gap-2 items-center">
                 <p>انتخاب ماه: </p>
                 <DatePicker
+                  value={date}
+                  onChange={(value) => {
+                    setDate(`${value.month}`)
+                  }}
                   onlyMonthPicker
                   format="MMMM YYYY"
                   calendar={persian}
@@ -67,12 +132,19 @@ export function Report() {
               </div>
               <div className="flex items-center gap-2 max-w-80">
                 <Label>نام کاربر :</Label>
-                <Input type="text" className="basis-24 h-9 w-40" />
+                <Input
+                  type="text"
+                  className="basis-24 h-9 w-40"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
               </div>
               <TooltipProvider>
                 <Tooltip>
                   <TooltipTrigger>
-                    <Button>دریافت</Button>
+                    <Button onClick={() => exportUserMonthReport()}>
+                      دریافت
+                    </Button>
                   </TooltipTrigger>
                   <TooltipContent>
                     <p>دریافت گزارش ماهانه کاربر</p>
@@ -90,6 +162,10 @@ export function Report() {
               <div className="flex gap-2 items-center">
                 <p>انتخاب ماه: </p>
                 <DatePicker
+                  value={date2}
+                  onChange={(value) => {
+                    setDate2(`${value.month}`)
+                  }}
                   onlyMonthPicker
                   format="MMMM YYYY"
                   calendar={persian}
@@ -111,7 +187,9 @@ export function Report() {
               <TooltipProvider>
                 <Tooltip>
                   <TooltipTrigger>
-                    <Button>دریافت</Button>
+                    <Button onClick={() => exportCompanyMonthReport()}>
+                      دریافت
+                    </Button>
                   </TooltipTrigger>
                   <TooltipContent>
                     <p>دریافت گزارش ماهانه شرکت</p>
